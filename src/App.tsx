9f83706d8efbad32583dfd235d318d5a93ee9a63
Youri Gruiters <i-youri-i@hotmail.com>
2022-08-17 15:35:57 +0200

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 type Props = {};
 
 const App: React.FC<Props> = () => {
-  return <h1>Gym app</h1>;
+  return <h1 className="text-5xl font-bold underline">Gym app</h1>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.(sa|sc|c)ss$/, // styles files
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
