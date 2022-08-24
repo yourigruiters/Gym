b64ae14d722a2b3ebf0a3012885f3043121a8f09
Youri Gruiters <i-youri-i@hotmail.com>
2022-08-24 11:26:10 +0200

    Provide message on email reset request

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -12,15 +12,14 @@ interface Props {}
 const ForgotPassword: React.FC<Props> = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+  const [requestSend, setRequestSend] = useState(false);
 
   const handleReset = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     sendPasswordResetEmail(auth, email)
-      .then((userCredential) => {
-        // Will be handled - Provide message in screen and remove option to do it again.
-        // CHECK SPAM FOLDER
-        console.log(userCredential);
+      .then(() => {
+        setRequestSend(true);
       })
       .catch((err) => {
         const errorCode = err.code;
@@ -50,25 +49,34 @@ const ForgotPassword: React.FC<Props> = () => {
         <img src={Logo} alt="logo" className="w-16 select-none pb-2" />
         <h1 className="text-2xl font-semibold">Fitness Tracking</h1>
       </div>
-      <form
-        className="flex flex-col pb-10 items-center gap-y-4 w-full"
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleReset(e)}
-      >
-        {error && <Warning message={error} onClick={resetError} />}
-        <Input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e: React.FormEvent<HTMLInputElement>) =>
-            setEmail(e.currentTarget.value)
-          }
-        />
-        <Button
-          type="submit"
-          classDecider="submit"
-          value="Request password reset"
-        />
-      </form>
+      {!requestSend ? (
+        <form
+          className="flex flex-col pb-10 items-center gap-y-4 w-full"
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleReset(e)}
+        >
+          {error && <Warning message={error} onClick={resetError} />}
+          <Input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e: React.FormEvent<HTMLInputElement>) =>
+              setEmail(e.currentTarget.value)
+            }
+          />
+          <Button
+            type="submit"
+            classDecider="submit"
+            value="Request password reset"
+          />
+        </form>
+      ) : (
+        <div className="flex flex-col pb-10 items-center gap-y-4 w-full">
+          <p className="text-center">
+            A password request email has been send to your provided email.
+            Please make sure to check your spam box.
+          </p>
+        </div>
+      )}
       <div className="flex flex-row items-center justify-between w-full">
         <p>Already have an account?</p>
 
